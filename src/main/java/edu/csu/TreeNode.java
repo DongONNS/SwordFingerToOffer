d1d7b9c6dc5e6d58a98d1b344c74dc0a46f68c8c
dongcheng2019 <dongcheng_2018@163.com>
2020-04-12 10:13:09 +0800

    is Sub Structure

---
+++ src/main/java/edu/csu/TreeNode.java
@@ -0,0 +1,10 @@
+package edu.csu;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val){
+        this.val = val;
+    }
+}
