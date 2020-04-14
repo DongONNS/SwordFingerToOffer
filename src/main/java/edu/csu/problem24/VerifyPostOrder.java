c981d34b6f64b71e8ed16fc78dccdbfc7c1b356e
dongcheng2019 <dongcheng_2018@163.com>
2020-04-14 22:07:17 +0800

    copy a complex list

---
+++ src/main/java/edu/csu/problem24/VerifyPostOrder.java
@@ -0,0 +1,15 @@
+package edu.csu.problem24;
+
+public class VerifyPostOrder {
+    public boolean verifyPostorder(int[] postorder) {
+        return recur(postorder,0,postorder.length - 1);
+    }
+    private boolean recur(int[] postorder,int left,int right){
+        if(left >= right) return true;
+        int point = left;
+        while(postorder[point] < postorder[right]) point++;
+        int part = point;
+        while(postorder[point] > postorder[right]) point++;
+        return point==right && recur(postorder,left,part-1) && recur(postorder,part,point-1);
+    }
+}
