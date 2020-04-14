8f1685a4d0b78d05017155f40ae04a05ccfda3b8
dongcheng2019 <dongcheng_2018@163.com>
2020-04-14 15:55:47 +0800

    recursive

---
+++ src/main/java/edu/csu/problem23/LevelOrder2Recursive.java
@@ -0,0 +1,23 @@
+package edu.csu.problem23;
+
+import edu.csu.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LevelOrder2Recursive {
+    List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> levelOrder(TreeNode root){
+        recur(root,0);
+        return res;
+    }
+    public void recur(TreeNode root,int k){
+        if(root != null){
+            if(res.size() <= k)
+                res.add(new ArrayList());
+            res.get(k).add(root.val);
+            recur(root.left,k+1);
+            recur(root.right,k+1);
+        }
+    }
+}
