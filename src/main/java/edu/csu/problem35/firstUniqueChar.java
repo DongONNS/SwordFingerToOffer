6acc9e85c6833a9e4b0d9eba3589ba15635f87dd
dongcheng2019 <dongcheng_2018@163.com>
2020-04-28 17:31:18 +0800

    first unique char

--- src/main/java/edu/csu/problem35/firstUniqueChar.java
+++ src/main/java/edu/csu/problem35/firstUniqueChar.java
@@ -7,28 +7,64 @@ import java.util.Map;
 
 public class firstUniqueChar {
     public static void main(String[] args) {
-        String s = "abbaccddeeeff";
+        String s = "leetcode";
         char c = firstUniqChar(s);
         System.out.println(c);
     }
-    public static char firstUniqChar(String s) {
+
+    public static char firstUniqChar(String s){
+        Map<Character, Boolean> dic = new HashMap<>();
         char[] array = s.toCharArray();
-        Map<Character,Integer> map = new HashMap();
-        for(int i = 0;i < array.length;i++){
-            char c = array[i];
-            if(map.containsKey(c)){
-                map.put(c,map.get(c)+1);
-            }else{
-                map.put(c,1);
-            }
+        for (char c : array) {
+            dic.put(c, !dic.containsKey(c));
         }
-        Set set = map.keySet();
-        Iterator iterator = set.iterator();
-        while(iterator.hasNext()){
-            char ch = (char)iterator.next();
-            int count = map.get(ch);
-            if(count == 1) return ch;
+        for (char c : array) {
+            if (dic.get(c)) return c;
         }
         return ' ';
     }
+
+
+//    public static char firstUniqChar(String s) {
+//        char[] array = s.toCharArray();
+//        Map<Character,Integer> map = new HashMap();
+//        for(int i = 0;i < array.length;i++){
+//            char c = array[i];
+//            if(map.containsKey(c)){
+//                map.put(c,map.get(c)+1);
+//            }else{
+//                map.put(c,1);
+//            }
+//        }
+//        for(int i = 0;i < array.length;i++){
+//            char c = array[i];
+//            if(map.get(c) == 1){
+//                return c;
+//            }
+//        }
+//        return ' ';
+//    }
+
+
+
+//    public static char firstUniqChar(String s) {
+//        char[] array = s.toCharArray();
+//        Map<Character,Integer> map = new HashMap();
+//        for(int i = 0;i < array.length;i++){
+//            char c = array[i];
+//            if(map.containsKey(c)){
+//                map.put(c,map.get(c)+1);
+//            }else{
+//                map.put(c,1);
+//            }
+//        }
+//        Set set = map.keySet();
+//        Iterator iterator = set.iterator();
+//        while(iterator.hasNext()){
+//            char ch = (char)iterator.next();
+//            int count = map.get(ch);
+//            if(count == 1) return ch;
+//        }
+//        return ' ';
+//    }
 }
