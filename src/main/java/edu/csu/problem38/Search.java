3454e525f684e941d3e9389280bc7727734d96b3
dongcheng2019 <dongcheng_2018@163.com>
2020-05-08 23:52:42 +0800

    search

---
+++ src/main/java/edu/csu/problem38/Search.java
@@ -0,0 +1,25 @@
+package edu.csu.problem38;
+
+public class Search {
+    public int search(int[] nums, int target) {
+        // 搜索右边界 right
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] <= target) i = m + 1;
+            else j = m - 1;
+        }
+        int right = i;
+        // 若数组中无 target ，则提前返回
+        if(j >= 0 && nums[j] != target) return 0;
+        // 搜索左边界 right
+        i = 0; j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] < target) i = m + 1;
+            else j = m - 1;
+        }
+        int left = j;
+        return right - left - 1;
+    }
+}
