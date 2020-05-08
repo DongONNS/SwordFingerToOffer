ac0465f45d8942719c8407d719a0982c7fe4c591
dongcheng2019 <dongcheng_2018@163.com>
2020-05-08 23:19:34 +0800

    resolv with the XOR operation

--- src/main/java/edu/csu/problem38/SingleNumbers.java
+++ src/main/java/edu/csu/problem38/SingleNumbers.java
@@ -9,22 +9,41 @@ public class SingleNumbers {
         int[] result = singleNumbers(nums);
         System.out.println(Arrays.toString(result));
     }
+
     public static int[] singleNumbers(int[] nums) {
-        if(nums.length == 0 || nums == null) return new int[0];
-        HashMap<Integer,Boolean> map = new HashMap();
-        for(int i = 0;i < nums.length;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],true);
-            }else{
-                map.put(nums[i],false);
-            }
-        }
+        int sum = 0;
         int[] result = new int[2];
-        int count = 0;
-        for(int i = 0;i < nums.length;i++){
-            if(map.get(nums[i]) == true)
-                result[count++] = nums[i];
+        for(int num : nums){
+            sum ^= num;
+        }
+        int lowbit = sum & (-sum);
+        for(int num : nums){
+            if((num & lowbit) == 0)
+                result[0] ^= num;
+            else
+                result[1] ^= num;
         }
         return result;
     }
+
+
+
+//    public static int[] singleNumbers(int[] nums) {
+//        if(nums.length == 0 || nums == null) return new int[0];
+//        HashMap<Integer,Boolean> map = new HashMap();
+//        for(int i = 0;i < nums.length;i++){
+//            if(!map.containsKey(nums[i])){
+//                map.put(nums[i],true);
+//            }else{
+//                map.put(nums[i],false);
+//            }
+//        }
+//        int[] result = new int[2];
+//        int count = 0;
+//        for(int i = 0;i < nums.length;i++){
+//            if(map.get(nums[i]) == true)
+//                result[count++] = nums[i];
+//        }
+//        return result;
+//    }
 }
