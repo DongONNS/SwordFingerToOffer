1f568db0e97591da41dd638c349daa77975389d6
dongcheng2019 <dongcheng_2018@163.com>
2020-05-09 23:36:02 +0800

    max depth,resolve with recurse

---
+++ src/main/java/edu/csu/problem39/MaxDepth.java
@@ -0,0 +1,12 @@
+package edu.csu.problem39;
+
+import edu.csu.TreeNode;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root){
+        if (root == null) return 0;
+        int leftLen = maxDepth(root.left);
+        int rightLen = maxDepth(root.right);
+        return Math.max(leftLen,rightLen)+1;
+    }
+}
