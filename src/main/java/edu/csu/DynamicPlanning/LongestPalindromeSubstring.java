293ae27e80f245d02df663dbbb5790a4784aa5c5
dongcheng2019 <dongcheng_2018@163.com>
2020-05-22 10:40:33 +0800

    最长回文子序列

---
+++ src/main/java/edu/csu/DynamicPlanning/LongestPalindromeSubstring.java
@@ -0,0 +1,31 @@
+package edu.csu.DynamicPlanning;
+
+import java.util.Arrays;
+
+public class LongestPalindromeSubstring {
+    public static void main(String[] args) {
+        String string = "a";
+        int res = longestPalindromeSubseq(string);
+        System.out.println(res);
+    }
+    public static int longestPalindromeSubseq(String s) {
+        if(s.length()==0) return 0;
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        for(int i = 0;i < len;i++){
+            Arrays.fill(dp[i],0);
+        }
+        //只有一个字符时，回文子序列的长度为1;
+        for(int i = 0;i < len;i++){
+            dp[i][i] = 1;
+        }
+        //泛着遍历得到结果
+        for(int i = len - 1;i >= 0;i--){
+            for(int j = i+1;j < len;j++){
+                if(s.charAt(i) == s.charAt(j)) dp[i][j]= dp[i+1][j-1]+2;
+                else dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+            }
+        }
+        return dp[0][len-1];
+    }
+}
