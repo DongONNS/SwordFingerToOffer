652206d1f2032f3813e1d196f09506f952c9d4fa
dongcheng2019 <dongcheng_2018@163.com>
2020-05-31 11:37:52 +0800

    find peak element 二分查找

---
+++ src/main/java/edu/csu/Top300/FindPeakElement2.java
@@ -0,0 +1,16 @@
+package edu.csu.Top300;
+
+public class FindPeakElement2 {
+    public int findPeakElement(int[] nums) {
+        return findPeakElement(nums,0,nums.length-1);
+    }
+    public int findPeakElement(int[] nums,int left,int right){
+        while(left < right){
+            int mid = (left + right)>>1;
+            //如果mid处元素处于上升子序列，那么peak在右侧[mid+1,right]之间
+            if(nums[mid] < nums[mid+1]) left = mid+1;
+            else right = mid;
+        }
+        return left;
+    }
+}
