4712c2fd0b9a15daee0aa1c6125528272b90423b
dongcheng2019 <dongcheng_2018@163.com>
2020-06-14 18:53:44 +0800

    integer break

---
+++ src/main/java/edu/csu/DynamicPlanning/NumArray.java
@@ -0,0 +1,24 @@
+package edu.csu.DynamicPlanning;
+
+public class NumArray {
+    int[] sum;
+    public NumArray(int[] nums) {
+        sum = new int[nums.length];
+        sum[0] = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            sum[i] = sum[i-1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if(i == 0) return sum[j];
+        else return sum[j] - sum[i-1];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+        NumArray numArray = new NumArray(nums);
+        int res = numArray.sumRange(2, 5);
+        System.out.println(res);
+    }
+}
