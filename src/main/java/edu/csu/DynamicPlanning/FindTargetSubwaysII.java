dd7c97241cbf066f33d0c1cdba0da4372dcbad75
dongcheng2019 <dongcheng_2018@163.com>
2020-06-17 16:58:57 +0800

    find max form

---
+++ src/main/java/edu/csu/DynamicPlanning/FindTargetSubwaysII.java
@@ -0,0 +1,15 @@
+package edu.csu.DynamicPlanning;
+
+public class FindTargetSubwaysII {
+    public int findTargetSumWays(int[] nums, int S) {
+        return findTargetSumWays(nums, 0, S);
+    }
+
+    private int findTargetSumWays(int[] nums, int start, int S) {
+        if (start == nums.length) {
+            return S == 0 ? 1 : 0;
+        }
+        return findTargetSumWays(nums, start + 1, S + nums[start])
+                + findTargetSumWays(nums, start + 1, S - nums[start]);
+    }
+}
