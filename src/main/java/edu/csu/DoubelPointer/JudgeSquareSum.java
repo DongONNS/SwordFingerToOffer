b67721de08457ab0fd31841ab4f6db9808cfb363
dongcheng2019 <dongcheng_2018@163.com>
2020-06-19 11:44:35 +0800

    judge square sum

---
+++ src/main/java/edu/csu/DoubelPointer/JudgeSquareSum.java
@@ -0,0 +1,15 @@
+package edu.csu.DoubelPointer;
+
+public class JudgeSquareSum {
+    public boolean judgeSquareSum(int target){
+        if (target < 0) return false;
+        int left = 0,right = (int)Math.sqrt(target);
+        while(left <= right){
+            int sum = left*left + right*right;
+            if (sum == target) return true;
+            else if (sum < target) left++;
+            else right--;
+        }
+        return false;
+    }
+}
