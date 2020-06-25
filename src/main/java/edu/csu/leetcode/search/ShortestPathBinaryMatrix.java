7c1db46b9aff7e3120fbdac8cf21001b51bad561
dongcheng2019 <dongcheng_2018@163.com>
2020-06-25 11:10:29 +0800

    shortest path binary matrix

---
+++ src/main/java/edu/csu/leetcode/search/ShortestPathBinaryMatrix.java
@@ -0,0 +1,42 @@
+package edu.csu.leetcode.search;
+
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ShortestPathBinaryMatrix {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return -1;
+        }
+        int[][] direction = {{1, -1}, {1, 0}, {1, 1}, {0, -1}, {0, 1}, {-1, -1}, {-1, 0}, {-1, 1}};
+        int m = grid.length, n = grid[0].length;
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair<>(0, 0));
+        int pathLength = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            pathLength++;
+            while (size-- > 0) {
+                Pair<Integer, Integer> cur = queue.poll();
+                int cr = cur.getKey(), cc = cur.getValue();
+                if (grid[cr][cc] == 1) {
+                    continue;
+                }
+                if (cr == m - 1 && cc == n - 1) {
+                    return pathLength;
+                }
+                grid[cr][cc] = 1; // 标记
+                for (int[] d : direction) {
+                    int nr = cr + d[0], nc = cc + d[1];
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                        continue;
+                    }
+                    queue.add(new Pair<>(nr, nc));
+                }
+            }
+        }
+        return -1;
+    }
+}
