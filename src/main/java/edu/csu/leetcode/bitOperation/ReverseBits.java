37de9e01b1109244c04b3af1d48d762100cbaa39
dongcheng2019 <dongcheng_2018@163.com>
2020-08-03 14:13:20 +0800

    reverse bits

---
+++ src/main/java/edu/csu/leetcode/bitOperation/ReverseBits.java
@@ -0,0 +1,11 @@
+package edu.csu.leetcode.bitOperation;
+
+public class ReverseBits {
+    public int reverseBits(int n){
+        int ret = 0;
+        for(int i = 31;i >= 0;i--){
+            ret = ret | ((n >> (31 - i)) & 1) << i;
+        }
+        return ret;
+    }
+}
