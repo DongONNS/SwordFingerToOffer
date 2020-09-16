ac1edd570ff06433735c7860434f807395c40f70
dongcheng2019 <dongcheng_2018@163.com>
2020-09-16 16:34:04 +0800

    trap solution

---
+++ src/main/java/edu/csu/leetcode/hot100/Trap_solution4.java
@@ -0,0 +1,25 @@
+package edu.csu.leetcode.hot100;
+
+public class Trap_solution4 {
+    public int trap(int[] height){
+        int sum = 0;
+
+        int max_left = 0;
+
+        // 统计i位置右边的最大高度--max_right
+        int[] max_right = new int[height.length];
+        for(int i = height.length - 2;i >= 0;i--){
+            max_right[i] = Math.max(max_right[i + 1],height[i + 1]);
+        }
+
+        for(int i = 1;i < height.length - 1;i++){
+            max_left = Math.max(max_left,height[i - 1]);
+
+            int min = Math.min(max_left,max_right[i]);
+
+            if(min > height[i])
+                sum += (min - height[i]);
+        }
+        return sum;
+    }
+}
---
+++ src/main/java/edu/csu/leetcode/hot100/Trap_solution5.java
@@ -0,0 +1,35 @@
+package edu.csu.leetcode.hot100;
+
+import java.util.Stack;
+
+public class Trap_solution5 {
+     public int trap(int[] height){
+         int sum = 0;
+         Stack<Integer> stack = new Stack();
+         int current = 0;
+         while(current < height.length){
+
+             // 当栈不为空并且当前位置墙的高度大于栈顶索引位置墙的高度
+             while(!stack.isEmpty() && height[current] > height[stack.peek()]){
+                 // 取出要出栈的元素
+                 int h = height[stack.peek()];
+
+                 // 出栈
+                 stack.pop();
+
+                 // 如果栈为空，那就直接出栈
+                 if(stack.isEmpty())
+                     break;
+
+                 // 计算两堵墙之间的距离
+                 int distance = current - stack.peek() - 1;
+                 int min = Math.min(height[stack.peek()],height[current]);
+                 sum += distance * (min - h);
+             }
+             // 将当前索引加入到队列中
+             stack.push(current);
+             current++;
+         }
+         return sum;
+     }
+}
