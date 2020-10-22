65f4a4902501f7f101dd28f2a6ac497679908c23
dongcheng2019 <dongcheng_2018@163.com>
2020-10-22 23:58:40 +0800

    right side view

---
+++ src/main/java/edu/csu/leetcode/hot100/RightSideView.java
@@ -0,0 +1,28 @@
+package edu.csu.leetcode.hot100;
+
+import edu.csu.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RightSideView {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        dfs(root, 0); // 从根节点开始访问，根节点深度是0
+        return res;
+    }
+
+    private void dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        // 先访问 当前节点，再递归地访问 右子树 和 左子树。
+        if (depth == res.size()) {   // 如果当前节点所在深度还没有出现在res里，说明在该深度下当前节点是第一个被访问的节点，因此将当前节点加入res中。
+            res.add(root.val);
+        }
+        depth++;
+        dfs(root.right, depth);
+        dfs(root.left, depth);
+    }
+}
