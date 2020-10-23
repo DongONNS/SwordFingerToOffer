1db38d464f0ad9ea906a32543cad5ed8e50fe1a8
dongcheng2019 <dongcheng_2018@163.com>
2020-10-23 23:51:20 +0800

    hindex

---
+++ src/main/java/edu/csu/leetcode/hot100/HIndex.java
@@ -0,0 +1,16 @@
+package edu.csu.leetcode.hot100;
+
+public class HIndex {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // 计数
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // 找出最大的 k
+        int k = n;
+        for (int s = papers[n]; k > s; s += papers[k])
+            k--;
+        return k;
+    }
+}
