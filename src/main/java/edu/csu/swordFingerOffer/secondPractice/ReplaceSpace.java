48aaa3364bc396e38c2e232e1dacd65e080df7df
dongcheng2019 <dongcheng_2018@163.com>
2020-11-13 23:47:56 +0800

    replace space

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/ReplaceSpace.java
@@ -0,0 +1,23 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+public class ReplaceSpace {
+    public String replaceSpace(StringBuffer str) {
+        int P1 = str.length() - 1;
+        for (int i = 0; i <= P1; i++)
+            if (str.charAt(i) == ' ')
+                str.append("  ");
+
+        int P2 = str.length() - 1;
+        while (P1 >= 0 && P2 > P1) {
+            char c = str.charAt(P1--);
+            if (c == ' ') {
+                str.setCharAt(P2--, '0');
+                str.setCharAt(P2--, '2');
+                str.setCharAt(P2--, '%');
+            } else {
+                str.setCharAt(P2--, c);
+            }
+        }
+        return str.toString();
+    }
+}
