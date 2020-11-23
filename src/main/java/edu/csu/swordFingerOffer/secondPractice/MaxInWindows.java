e31ae1dc3695b3ade6d3f9f30b77b2de6e92ee9b
dongcheng2019 <dongcheng_2018@163.com>
2020-11-23 23:43:44 +0800

    max in windows

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/MaxInWindows.java
@@ -0,0 +1,22 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class MaxInWindows {
+    public ArrayList<Integer> maxInWindows(int[] num, int size) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        if (size > num.length || size < 1)
+            return ret;
+        PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> o2 - o1);  /* 大顶堆 */
+        for (int i = 0; i < size; i++)
+            heap.add(num[i]);
+        ret.add(heap.peek());
+        for (int i = 0, j = i + size; j < num.length; i++, j++) {            /* 维护一个大小为 size 的大顶堆 */
+            heap.remove(num[i]);
+            heap.add(num[j]);
+            ret.add(heap.peek());
+        }
+        return ret;
+    }
+}
