275a785a519793e60c3629baf70e7f252f8c6dee
dongcheng2019 <dongcheng_2018@163.com>
2020-11-25 23:51:05 +0800

    find Continuous Sequence

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/findContinuousSequence.java
@@ -0,0 +1,30 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+import java.util.ArrayList;
+
+public class findContinuousSequence {
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
+        int start = 1, end = 2;
+        int curSum = 3;
+        while (end < sum) {
+            if (curSum > sum) {
+                curSum -= start;
+                start++;
+            } else if (curSum < sum) {
+                end++;
+                curSum += end;
+            } else {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int i = start; i <= end; i++)
+                    list.add(i);
+                ret.add(list);
+                curSum -= start;
+                start++;
+                end++;
+                curSum += end;
+            }
+        }
+        return ret;
+    }
+}
