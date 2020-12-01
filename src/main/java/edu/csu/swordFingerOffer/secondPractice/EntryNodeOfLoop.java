910ed8b07a41f5ab57c4d0c56e2a53f6fe662662
dongcheng2019 <dongcheng_2018@163.com>
2020-12-01 23:48:38 +0800

    entry node of loop

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/EntryNodeOfLoop.java
@@ -0,0 +1,21 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+import edu.csu.ListNode;
+
+public class EntryNodeOfLoop {
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        if (pHead == null || pHead.next == null)
+            return null;
+        ListNode slow = pHead, fast = pHead;
+        do {
+            fast = fast.next.next;
+            slow = slow.next;
+        } while (slow != fast);
+        fast = pHead;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
