12646c1d492bcad097e17d005b37cb1218547ea8
dongcheng2019 <dongcheng_2018@163.com>
2020-12-09 23:30:54 +0800

    height

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/Height.java
@@ -0,0 +1,22 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+import edu.csu.TreeNode;
+
+public class Height {
+    private boolean isBalanced = true;
+
+    public boolean IsBalanced_Solution(TreeNode root) {
+        height(root);
+        return isBalanced;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null || !isBalanced)
+            return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if (Math.abs(left - right) > 1)
+            isBalanced = false;
+        return 1 + Math.max(left, right);
+    }
+}
