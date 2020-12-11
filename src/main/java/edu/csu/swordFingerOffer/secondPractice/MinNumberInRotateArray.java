509c2b31fdc7c03be0d4771cdb2a9518d13f8a71
dongcheng2019 <dongcheng_2018@163.com>
2020-12-11 23:10:24 +0800

    min number in rotate array

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/MinNumberInRotateArray.java
@@ -0,0 +1,17 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+public class MinNumberInRotateArray {
+    public int minNumberInRotateArray(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] <= nums[h])
+                h = m;
+            else
+                l = m + 1;
+        }
+        return nums[l];
+    }
+}
