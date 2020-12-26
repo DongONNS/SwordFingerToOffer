a5b927d7c2efeae45c992151ed4e5403328b364d
dongcheng2019 <dongcheng_2018@163.com>
2020-12-26 23:40:21 +0800

    get most

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/GetMost.java
@@ -0,0 +1,16 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+public class GetMost {
+    public int getMost(int[][] values) {
+        if (values == null || values.length == 0 || values[0].length == 0)
+            return 0;
+        int n = values[0].length;
+        int[] dp = new int[n];
+        for (int[] value : values) {
+            dp[0] += value[0];
+            for (int i = 1; i < n; i++)
+                dp[i] = Math.max(dp[i], dp[i - 1]) + value[i];
+        }
+        return dp[n - 1];
+    }
+}
