314edba29853719ac9a224b29bc4442f27028828
dongcheng2019 <dongcheng_2018@163.com>
2021-01-01 23:34:28 +0800

    is numeric

---
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/IsNumeric.java
@@ -0,0 +1,9 @@
+package edu.csu.swordFingerOffer.secondPractice;
+
+public class IsNumeric {
+    public boolean isNumeric(char[] str) {
+        if (str == null || str.length == 0)
+            return false;
+        return new String(str).matches("[+-]?\\d*(\\.\\d+)?([eE][+-]?\\d+)?");
+    }
+}
--- src/main/java/edu/csu/swordFingerOffer/secondPractice/Match.java
+++ src/main/java/edu/csu/swordFingerOffer/secondPractice/Match.java
@@ -4,6 +4,7 @@ public class Match {
     public boolean match(char[] str, char[] pattern) {
 
         int m = str.length, n = pattern.length;
+        // dp[i][j]表示长度为i的字符串能否由长度为j的pattern组成
         boolean[][] dp = new boolean[m + 1][n + 1];
 
         dp[0][0] = true;
@@ -13,9 +14,12 @@ public class Match {
 
         for (int i = 1; i <= m; i++)
             for (int j = 1; j <= n; j++)
+                // 如果str[i - 1] 与 pattern[j - 1]匹配上了，那么由前面的结果决定
                 if (str[i - 1] == pattern[j - 1] || pattern[j - 1] == '.')
                     dp[i][j] = dp[i - 1][j - 1];
+                // 当前符号为*时候
                 else if (pattern[j - 1] == '*')
+                    //如果能由上一个字符串匹配上
                     if (pattern[j - 2] == str[i - 1] || pattern[j - 2] == '.') {
                         dp[i][j] |= dp[i][j - 1]; // a* counts as single a
                         dp[i][j] |= dp[i - 1][j]; // a* counts as multiple a
